87305164bf795920bc6d6dc3311c9a5846b27690
Amir7RN <133896290+Amir7RN@users.noreply.github.com>
2023-10-28 22:41:37 -0400

    Add files via upload

---
+++ IMUDATA_SPI_EtherCatSlave/build/applications/raspberry_lan9252demo/CMakeFiles/GetSensorData.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GetSensorData.dir/main.c.o"
+  "CMakeFiles/GetSensorData.dir/slave_objectlist.c.o"
+  "GetSensorData"
+  "GetSensorData.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GetSensorData.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ IMUDATA_SPI_EtherCatSlave/build/applications/raspberry_lan9252demo/CMakeFiles/getSensorData_lib.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/IMU/FINALCODE/ONEIMU/applications/raspberry_lan9252demo/getSensorData.cpp" "/home/IMU/FINALCODE/ONEIMU/build/applications/raspberry_lan9252demo/CMakeFiles/getSensorData_lib.dir/getSensorData.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../soes"
+  "../soes/include/sys/gcc"
+  "../applications/raspberry_lan9252demo"
+  "/usr/share/c++-mscl"
+  "/usr/share/c++-mscl/source"
+  "/usr/share/c++-mscl/Boost"
+  "/usr/share/c++-mscl/Boost/include/boost"
+  "/usr/share/c++-mscl/Boost/lib"
+  "/usr/share/c++-mscl/source/mscl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
