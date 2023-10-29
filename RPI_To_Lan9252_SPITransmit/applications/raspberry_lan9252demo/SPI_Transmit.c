bde736c5fdef9251e3d264217461c650770b57e8
Amir7RN <133896290+Amir7RN@users.noreply.github.com>
2023-10-28 22:33:43 -0400

    Update SPI_Transmit.c

--- RPI_To_Lan9252_SPITransmit/applications/raspberry_lan9252demo/SPI_Transmit.c
+++ RPI_To_Lan9252_SPITransmit/applications/raspberry_lan9252demo/SPI_Transmit.c
@@ -11,89 +11,89 @@ _Mbuffer Mb;
 
 void cb_get_inputs(void)
 {
-
+    // Callback for retrieving inputs (optional)
 }
 
 void cb_set_outputs(void)
 {
-
+    // Callback for setting outputs (optional)
 }
 
-
-int16_t generate_random_negative_value(void)
+// Function to generate a random value in the range [-100, 100]
+int16_t generate_random_value(void)
 {
-
-   return (int16_t)(rand() % 201 - 100);
+    return (int16_t)(rand() % 201 - 100);
 }
 
-int8_t received_data = 0; // Global variable
+int8_t received_data = 0; // Global variable to store received data
 
+// Function to transmit data via SPI
 void SPI_transmit(int16_t data)
 {
-   printf("Transmitting Value: %d\n", data);
-   int8_t send_data = (int8_t)(data);
-   int8_t received_data_unit = (int8_t)bcm2835_spi_transfer((uint8_t)send_data);
-   received_data = received_data_unit;
-   int16_t processed_data = (int16_t)(received_data);
-   printf("Received Value: %d\n", processed_data);
+    printf("Transmitting Value: %d\n", data);
+    int8_t send_data = (int8_t)(data);
+    int8_t received_data_unit = (int8_t)bcm2835_spi_transfer((uint8_t)send_data);
+    received_data = received_data_unit;
+    int16_t processed_data = (int16_t)(received_data);
+    printf("Received Value: %d\n", processed_data);
 }
 
+// Callback for transmitting data
 void cb_transmit(void)
 {
-   // Transmit random negative value via SPI
-   int16_t transmit_value;
-   
-   do {
-      transmit_value = generate_random_negative_value();
-   } while (transmit_value == 56);
-   
-      
-   SPI_transmit(transmit_value);
+    // Transmit random negative value via SPI
+    int16_t transmit_value;
+    
+    do {
+        transmit_value = generate_random_negative_value();
+    } while (transmit_value == 56);
+    
+    SPI_transmit(transmit_value);
 
-   // Receive and interpret the received data
-   int16_t received_value = (int16_t)(received_data & 0xFF); // Convert the received data to signed value
-   if (received_value >127){
-      received_value -=256;
-   }
-   Obj.in.IMU1_Pitch = (int16_t)transmit_value;
-   
+    // Receive and interpret the received data
+    int16_t received_value = (int16_t)(received_data & 0xFF); // Convert the received data to a signed value
+    if (received_value > 127) {
+        received_value -= 256;
+    }
+    Obj.in.IMU1_Pitch = (int16_t)transmit_value;
 }
 
+// Main run function
 int main_run(void *arg)
 {
-   static esc_cfg_t config =
-   {
-      .user_arg = "rpi4,cs1",
-      .use_interrupt = 0,
-      .watchdog_cnt = 150,
-      .set_defaults_hook = NULL,
-      .pre_state_change_hook = NULL,
-      .post_state_change_hook = NULL,
-      .application_hook = NULL,
-      .safeoutput_override = NULL,
-      .pre_object_download_hook = NULL,
-      .post_object_download_hook = NULL,
-      .rxpdo_override = NULL,
-      .txpdo_override = NULL,
-      .esc_hw_interrupt_enable = NULL,
-      .esc_hw_interrupt_disable = NULL,
-      .esc_hw_eep_handler = NULL,
-      .esc_check_dc_handler = NULL,
-   };
+    static esc_cfg_t config =
+    {
+        .user_arg = "rpi4,cs1",
+        .use_interrupt = 0,
+        .watchdog_cnt = 150,
+        .set_defaults_hook = NULL,
+        .pre_state_change_hook = NULL,
+        .post_state_change_hook = NULL,
+        .application_hook = NULL,
+        .safeoutput_override = NULL,
+        .pre_object_download_hook = NULL,
+        .post_object_download_hook = NULL,
+        .rxpdo_override = NULL,
+        .txpdo_override = NULL,
+        .esc_hw_interrupt_enable = NULL,
+        .esc_hw_interrupt_disable = NULL,
+        .esc_hw_eep_handler = NULL,
+        .esc_check_dc_handler = NULL,
+    };
 
-      ecat_slv_init(&config);
+    ecat_slv_init(&config);
 
-   while (1)
-   {
-       ecat_slv();
-       cb_transmit();
-   }
+    while (1)
+    {
+        ecat_slv();
+        cb_transmit();
+    }
 
-   return 0;
+    return 0;
 }
 
 int main(void)
 {
-   main_run(NULL);
-   return 0;
+    main_run(NULL);
+    return 0;
 }
