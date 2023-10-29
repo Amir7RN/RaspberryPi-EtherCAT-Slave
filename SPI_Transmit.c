b411cf870990539608d9912d0a24eeec0c036721
Amir7RN <133896290+Amir7RN@users.noreply.github.com>
2023-10-28 21:41:48 -0400

    Add files via upload

---
+++ SPI_Transmit.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include "ecat_slv.h"
+#include "utypes.h"
+#include <bcm2835.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Application variables */
+_Objects Obj;
+_Mbuffer Mb;
+
+void cb_get_inputs(void)
+{
+
+}
+
+void cb_set_outputs(void)
+{
+
+}
+
+
+int16_t generate_random_negative_value(void)
+{
+
+   return (int16_t)(rand() % 201 - 100);
+}
+
+int8_t received_data = 0; // Global variable
+
+void SPI_transmit(int16_t data)
+{
+   printf("Transmitting Value: %d\n", data);
+   int8_t send_data = (int8_t)(data);
+   int8_t received_data_unit = (int8_t)bcm2835_spi_transfer((uint8_t)send_data);
+   received_data = received_data_unit;
+   int16_t processed_data = (int16_t)(received_data);
+   printf("Received Value: %d\n", processed_data);
+}
+
+void cb_transmit(void)
+{
+   // Transmit random negative value via SPI
+   int16_t transmit_value;
+   
+   do {
+      transmit_value = generate_random_negative_value();
+   } while (transmit_value == 56);
+   
+      
+   SPI_transmit(transmit_value);
+
+   // Receive and interpret the received data
+   int16_t received_value = (int16_t)(received_data & 0xFF); // Convert the received data to signed value
+   if (received_value >127){
+      received_value -=256;
+   }
+   Obj.in.IMU1_Pitch = (int16_t)transmit_value;
+   
+}
+
+int main_run(void *arg)
+{
+   static esc_cfg_t config =
+   {
+      .user_arg = "rpi4,cs1",
+      .use_interrupt = 0,
+      .watchdog_cnt = 150,
+      .set_defaults_hook = NULL,
+      .pre_state_change_hook = NULL,
+      .post_state_change_hook = NULL,
+      .application_hook = NULL,
+      .safeoutput_override = NULL,
+      .pre_object_download_hook = NULL,
+      .post_object_download_hook = NULL,
+      .rxpdo_override = NULL,
+      .txpdo_override = NULL,
+      .esc_hw_interrupt_enable = NULL,
+      .esc_hw_interrupt_disable = NULL,
+      .esc_hw_eep_handler = NULL,
+      .esc_check_dc_handler = NULL,
+   };
+
+      ecat_slv_init(&config);
+
+   while (1)
+   {
+       ecat_slv();
+       cb_transmit();
+   }
+
+   return 0;
+}
+
+int main(void)
+{
+   main_run(NULL);
+   return 0;
+}
